2cd557deb4a907deaea9e2589b357c532272a509
Brill Pappin <brill@pappin.ca>
2025-05-16 19:21:20 -0400

    Prep for release

--- datetime/src/commonTest/kotlin/io/github/aughtone/datetime/format/LocalTimeExtTest.kt
+++ datetime/src/commonTest/kotlin/io/github/aughtone/datetime/format/LocalTimeExtTest.kt
@@ -4,18 +4,11 @@ import androidx.compose.ui.text.intl.Locale
 import kotlinx.datetime.LocalTime
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class LocalTimeExtTest {
     val testTime1 = LocalTime(16,8,39)
     val testLocal = Locale("en-CA")
-
-//    @Test
-//    fun testTimeZoneIds() {
-//        TimeZone.availableZoneIds.forEach {
-//            println(it)
-//        }
-//        fail("stop the test")
-//    }
 
     @Test
     fun testFormatShort() {
@@ -31,15 +24,21 @@ class LocalTimeExtTest {
 
     @Test
     fun testFormatLong() {
-        assertEquals("4:08:39 p.m. EST", testTime1.format(DateTimeStyle.Long, locale = testLocal))
-        assertEquals("16:08:39 EST", testTime1.format(DateTimeStyle.Long, locale = testLocal, is24HourFormat = true))
+        val actual1= testTime1.format(DateTimeStyle.Long, locale = testLocal)
+        assertTrue(actual1 == "4:08:39 p.m. EST" || actual1 == "4:08:39 p.m. EDT" )
+
+        val actual2 = testTime1.format(DateTimeStyle.Long, locale = testLocal, is24HourFormat = true)
+        assertTrue(actual2 == "16:08:39 EST" || actual2 == "16:08:39 EDT" )
     }
 
     @Test
     fun testFormatFull() {
         // the new format seems to be "America/Toronto"
-        assertEquals("4:08:39 p.m. Eastern Standard Time", testTime1.format(DateTimeStyle.Full, locale = testLocal))
-        assertEquals("16:08:39 Eastern Standard Time", testTime1.format(DateTimeStyle.Full, locale = testLocal, is24HourFormat = true))
+        val actual1 = testTime1.format(DateTimeStyle.Full, locale = testLocal)
+        assertTrue(actual1 == "4:08:39 p.m. Eastern Standard Time" || actual1 == "4:08:39 p.m. Eastern Daylight Time" )
+
+        val actual2 = testTime1.format(DateTimeStyle.Full, locale = testLocal, is24HourFormat = true)
+        assertTrue(actual2 == "16:08:39 Eastern Standard Time" || actual2 == "16:08:39 Eastern Daylight Time" )
     }
     @Test
     fun testFormatNone() {
