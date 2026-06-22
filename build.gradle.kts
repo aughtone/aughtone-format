ec9a462fbae0157439be348765ff5271763b1314
Brill Pappin <brill@pappin.ca>
2026-06-22 08:42:09 -0400

    change yarn lock to warning

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
+import kotlin.apply
+
 plugins {
     alias(libs.plugins.multiplatformLibrary) apply false
     alias(libs.plugins.kotlinMultiplatform) apply  false
@@ -7,3 +12,11 @@ plugins {
     alias(libs.plugins.composeCompiler) apply false
 }
 
+rootProject.plugins.withType<YarnPlugin> {
+    rootProject.the<YarnRootExtension>().apply {
+        lockFileDirectory = project.rootDir.resolve("kotlin-js-store")
+        yarnLockMismatchReport = org.jetbrains.kotlin.gradle.targets.js.yarn.YarnLockMismatchReport.WARNING
+        reportNewYarnLock = false
+        yarnLockAutoReplace = true
+    }
+}
